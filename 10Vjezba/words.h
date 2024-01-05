3567ce716be6be01568a03bb6eb67a4a3bdb5ed9
Ela1009 <ela.vrcelj@gmail.com>
2024-01-05 18:46:19 +0100

    words.h

---
+++ 10Vjezba/words.h
@@ -0,0 +1,8 @@
+#ifndef WORDS_H
+#define WORDS_H
+
+#include <string>
+
+void words(const std::string& ime_datoteke, const std::string& rijec_koju_trazimo, const std::string& rijec_koju_uklanjamo);
+
+#endif // WORDS_H
